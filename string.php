cd6bd890a7d902fc3a440c927f72ba9820083810
korius kussa wenda <koriuswenda@gmail.com>
2023-09-19 20:48:40 +0700

    tugas tutorial

---
+++ string.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipe Data String</title>
+</head>
+<body>
+    
+<?php
+
+        //String
+        $nama = 'coding is fun ';
+
+        //Integer
+        $nama = 20;
+
+        //Float
+        $nama = 20.5784 ;
+
+        //Boolean
+
+    
+
+        //Arrayy
+        $names = array("Korius","Daniel","Michael");
+
+        echo $names[ ("2")
+
+         ];
+
+
+?>
+
+
+
+</body>
+</html>
